105e010935ee75a0d8e6f13800f3797e2ab0e99b
Ta Tonthongkam <theerasan@eatigo.com>
2017-09-14 00:38:29 +0700

    hide menu

--- app/components/nav/TopBar.jsx
+++ app/components/nav/TopBar.jsx
@@ -11,7 +11,6 @@ var TopBar = React.createClass({
           <li><a href="#main-form">ร่วมกิจกรรม</a></li>
           <li><a href="#reward">รางวัล</a></li>
           <li><a href="#live-wall">ดู Live Wall</a></li>
-          <li><a href="#video">เคล็ดลับสุขภาพดี</a></li>
           <li><a target="_blank" href="http://www.aia.com/en/about-aia/contact-us.html" ><img src="../img/mail.png"/></a></li>
         </ul>
         <ul id="phone-menu" className="menu align-right hide-for-large dropdown" data-dropdown-menu>
@@ -19,13 +18,12 @@ var TopBar = React.createClass({
             <a href="#">
               <img src="../img/ic_menu.png"/>
             </a>
-            <ul className="menu">
-                <li><a className="sub-menu" href="#">เกี่ยวกับ#WhatsYourWhy</a></li>
-                <li><a className="sub-menu" href="#">ร่วมกิจกรรม</a></li>
-                <li><a className="sub-menu" href="#">รางวัล</a></li>
-                <li><a className="sub-menu" href="#">ดู Live Wall</a></li>
-                <li><a className="sub-menu" href="#">เคล็ดลับสุขภาพดี</a></li>
-                <li><a className="sub-menu" href="#"><img src="../img/mail.png"/></a></li>
+            <ul id="phone-menu-list" className="menu" data-smooth-scroll data-animation-easing="swing" data-threshold="0">
+                <li><a className="sub-menu" href="#body-content">เกี่ยวกับ#WhatsYourWhy</a></li>
+                <li><a className="sub-menu" href="#main-form">ร่วมกิจกรรม</a></li>
+                <li><a className="sub-menu" href="#reward">รางวัล</a></li>
+                <li><a className="sub-menu" href="#live-wall">ดู Live Wall</a></li>
+                <li><a className="sub-menu" target="_blank" href="http://www.aia.com/en/about-aia/contact-us.html" ><img src="../img/mail.png"/></a></li>
             </ul>
           </li>
         </ul>
@@ -35,6 +33,7 @@ var TopBar = React.createClass({
   componentDidMount: function () {
     var elem = new Foundation.DropdownMenu($('#phone-menu'), null);
     var elem = new Foundation.SmoothScroll($('#desktop-menu'), null);
+    var elem = new Foundation.SmoothScroll($('#phone-menu-list'), null);
   }
 });
 
